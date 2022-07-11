4b70b00c135a0e67a9d94c6fb112913392e8b70f
Lalluviadel <inspiracion@yandex.ru>
2022-07-11 16:08:47 +0300

    - добавлен скрипт для быстрого создания суперпользователя, команда в терминале: python manage.py cr_su

---
+++ articles/management/commands/cr_su.py
@@ -0,0 +1,12 @@
+"""Contains custom commands for easy launch by manage.py."""
+from articles.models import HabrUser
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    """A command for quickly creating a superuser."""
+    def handle(self, *args, **options):
+        user = HabrUser.objects.create_superuser('admin', 'admin@mail.ru', '1', user_name='admin')
+        user.is_active = True
+        user.save()
