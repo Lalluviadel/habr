958b64684660637186cabc42a546cb4c68c587b7
Lalluviadel <inspiracion@yandex.ru>
2022-07-13 19:11:33 +0300

    - добавлен контроллер для вывода статей отдельной категории; - добавлен шаблон для вывода статей отдельной категории; - кнопки категории в меню теперь активны; - кнопка перейти к статьям категории на странице отдельной статьи теперь активна; - изменен дизайн кнопок пагинации;

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from articles.views import ArticleView
+from articles.views import ArticleView, CategoryView
 
 app_name = 'articles'
 urlpatterns = [
+    path('categories/<slug:category_slug>/', CategoryView.as_view(), name='category_read'),
     path('<slug:article_slug>/', ArticleView.as_view(), name='article_read'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import get_object_or_404
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 
-from articles.models import Article
+from articles.models import Article, Category
 
 
 class MainView(ListView):
@@ -15,6 +15,27 @@ class MainView(ListView):
         """Displaying all active articles.
          Articles are displayed in an orderly manner, starting from the most recent."""
         return Article.objects.filter(is_active=True)
+
+
+class CategoryView(ListView):
+    """View for articles of a separate category (section)."""
+    model = Article
+    template_name = 'articles/read_category.html'
+    context_object_name = 'category_articles'
+    paginate_by = 3
+    slug_url_kwarg = 'category_slug'
+    category = ''
+
+    def get_queryset(self):
+        """Returns a queryset of active articles of the specified category."""
+        self.category = Category.objects.get(slug=self.kwargs.get('category_slug'))
+        return Article.objects.filter(Q(category=self.category), Q(is_active=True))
+
+    def get_context_data(self, *args, **kwargs):
+        """Puts the title in the context according to the selected category."""
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Посты категории {self.category}'
+        return context
 
 
 class ArticleView(DetailView):
