7035cead4f349836227df78852d5002d6f1875e3
61003 <g100m8888@gmail.com>
2022-07-13 09:02:42 +0400

    admin model fix

--- articles/models.py
+++ articles/models.py
@@ -2,12 +2,9 @@
 Stores the user, article, category and tag models.
 """
 
-from datetime import datetime, timedelta
-from uuid import uuid4
-
-from django.contrib.auth.models import AbstractUser
+from datetime import datetime
 from django.db import models
-
+from userapp.models import HabrUser
 
 class Category(models.Model):
     """The model for the category."""
@@ -29,33 +26,6 @@ class Category(models.Model):
         verbose_name_plural = 'Разделы сайта'
 
 
-class HabrUser(AbstractUser):
-    """The model for the user."""
-    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    username = models.CharField(verbose_name="логин", max_length=128, blank=False, unique=True)
-    first_name = models.CharField(verbose_name="имя", max_length=128, blank=False)
-    last_name = models.CharField(verbose_name="фамилия", max_length=128, blank=False)
-    avatar = models.ImageField(upload_to="users_avatars", blank=True)
-    email = models.CharField(verbose_name="email", max_length=128, unique=True, blank=False)
-    age = models.PositiveIntegerField(verbose_name="возраст", default=18)
-    registration_time = models.DateTimeField(verbose_name="время регистрации", default=datetime.now)
-    is_active = models.BooleanField(verbose_name="пользователь активен", default=True, db_index=True)
-    activation_key = models.CharField(verbose_name="ключ подтверждения", max_length=128, blank=True)
-    activation_key_expires = models.DateTimeField(verbose_name="актуальность ключа",
-                                                  default=datetime.now() + timedelta(hours=48))
-
-    def __str__(self):
-        """Forms a printable representation of the object.
-        Returns the username of the user.
-        """
-        return str(self.username)
-
-    class Meta:
-        """Ordering users according to their id.
-        """
-        ordering = ('uuid',)
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
 
 
 class Tag(models.Model):
---
+++ userapp/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from uuid import uuid4
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime, timedelta
+
+class HabrUser(AbstractUser):
+    """The model for the user."""
+    uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    username = models.CharField(verbose_name="логин", max_length=128, blank=False, unique=True)
+    first_name = models.CharField(verbose_name="имя", max_length=128, blank=False)
+    last_name = models.CharField(verbose_name="фамилия", max_length=128, blank=False)
+    avatar = models.ImageField(upload_to="users_avatars", blank=True)
+    email = models.CharField(verbose_name="email", max_length=128, unique=True, blank=False)
+    age = models.PositiveIntegerField(verbose_name="возраст", default=18)
+    registration_time = models.DateTimeField(verbose_name="время регистрации", default=datetime.now)
+    is_active = models.BooleanField(verbose_name="пользователь активен", default=True, db_index=True)
+    activation_key = models.CharField(verbose_name="ключ подтверждения", max_length=128, blank=True)
+    activation_key_expires = models.DateTimeField(verbose_name="актуальность ключа",
+                                                  default=datetime.now() + timedelta(hours=48))
+
+    def __str__(self):
+        """Forms a printable representation of the object.
+        Returns the username of the user.
+        """
+        return str(self.username)
+
+    class Meta:
+        """Ordering users according to their id.
+        """
+        ordering = ('uuid',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
